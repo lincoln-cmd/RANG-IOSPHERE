577a4dc87dcfe3003c4f12d4bd1e7011781988a6
Lincoln <dhkim1551@naver.com>
2026-09-01 19:55:55 +0900

    feat: support accessible math notation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://rang-iosphere.pages.dev',
   integrations: [sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { strict: false }]],
+  },
 });
